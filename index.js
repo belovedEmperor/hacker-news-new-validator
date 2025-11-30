0a62f169ac08a5b370877af21b77c96297c40f8a
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2025-11-30 16:44:17 -0500

    Can get page posts

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// @ts-check
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
 
@@ -9,6 +10,33 @@ async function sortHackerNewsArticles() {
 
   // go to Hacker News
   await page.goto("https://news.ycombinator.com/newest");
+
+  console.log("Getting...");
+  await getPagePosts(page);
+  console.log("Got");
+
+  // await browser.close();
+}
+
+async function getPagePosts(page) {
+  try {
+    const table = page.locator("tr#bigbox table");
+
+    const titleRows = await table.locator("tr.athing").all();
+    for (const row of titleRows) {
+      const title = await row
+        .locator("span.titleline a[rel='nofollow']")
+        .textContent();
+
+      const subtextRow = row.locator("xpath=./following-sibling::tr[1]");
+      const subtext = await subtextRow.locator("td.subtext").textContent();
+
+      console.log(title);
+      console.log(subtext);
+    }
+  } catch (e) {
+    console.error(`Failed to get posts: \n${e}`);
+  }
 }
 
 (async () => {
