9d17c5236003ea1882a9f9794e75bf65c04a1a04
belovedEmperor <37455454+belovedEmperor@users.noreply.github.com>
2026-01-04 23:22:40 -0500

    Added date validation logic

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ async function sortHackerNewsArticles() {
   const context = await browser.newContext();
 
   const hackerNewsLink = "https://news.ycombinator.com";
+  const constTime = new Date();
 
   const page1 = await context.newPage();
   await page1.goto(`${hackerNewsLink}/newest`);
   console.log("\n\nGetting page 1");
-  const ages1 = await getPagePosts(page1, true);
-
-  // NOTE: NEED TO IMPLEMENT CHECK DATES WITHIN ARRAY & BETWEEN ARRAYS
+  const ages1 = await getPagePosts(page1, true, constTime);
 
   const link = await getNextPageLink(page1);
   const time = String(link.match(new RegExp("next=\\d+&")))
@@ -24,12 +23,14 @@ async function sortHackerNewsArticles() {
   console.log(`\n\n\n\n${time}\n\n\n\n`);
   await page1.close();
 
+  let ages2, ages3, ages4;
+
   const item1 = async () => {
     console.log("\n\nGetting page 2");
     const page2 = await context.newPage();
     await page2.goto(`${hackerNewsLink}/newest?next=${time}&n=31`);
     await page2.locator("span.rank").getByText("31").waitFor();
-    const ages2 = await getPagePosts(page2, true);
+    ages2 = await getPagePosts(page2, true, constTime);
     await page2.close();
   };
 
@@ -38,7 +39,7 @@ async function sortHackerNewsArticles() {
     const page3 = await context.newPage();
     await page3.goto(`${hackerNewsLink}/newest?next=${time}&n=61`);
     await page3.locator("span.rank").getByText("61").waitFor();
-    const ages3 = await getPagePosts(page3, true);
+    ages3 = await getPagePosts(page3, true, constTime);
     await page3.close();
   };
 
@@ -47,17 +48,42 @@ async function sortHackerNewsArticles() {
     const page4 = await context.newPage();
     await page4.goto(`${hackerNewsLink}/newest?next=${time}&n=91`);
     await page4.locator("span.rank").getByText("91").waitFor();
-    const ages4 = await getPagePosts(page4, false);
+    ages4 = await getPagePosts(page4, false, constTime);
     await page4.close();
   };
 
   await Promise.all([item1(), item2(), item3()]);
 
+  const ages = [...ages1, ...ages2, ...ages3, ...ages4];
+  const invalids = await validateDates(ages);
+  if (invalids.length !== 0) {
+    console.log("These errors posts are invalid:");
+    console.log("\n|id|          time          |");
+    for (const [id1, id2] of invalids) {
+      console.log(`|${id1}|${ages[id1].toISOString()}| <-- older`);
+      console.log(`|${id2}|${ages[id2].toISOString()}| <-- newer`);
+      console.log("|  |                        |");
+    }
+  } else {
+    console.log("All dates are valid");
+  }
+
   await context.close();
   await browser.close();
 }
 
-async function getPagePosts(page, all) {
+async function validateDates(dates) {
+  let invalids = [];
+
+  for (let i = 0; i < dates.length - 1; i++) {
+    if (dates[i] < dates[i + 1]) {
+      invalids.push([i, i + 1]);
+    }
+  }
+  return invalids;
+}
+
+async function getPagePosts(page, all, referenceTime) {
   try {
     const ages = [];
 
@@ -76,6 +102,7 @@ async function getPagePosts(page, all) {
       ages.push(
         chrono.parseDate(
           await subtextRow.locator("span.age").locator("a").textContent(),
+          referenceTime,
         ),
       );
 
